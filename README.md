88c35f9d8ee8c9005f02f5b1a08f416a130d1479
mgonzales7 <mgonzales7@fordham.edu>
2015-11-15 07:03:36 -0500

    commiting project to github

---
+++ README.md
@@ -0,0 +1,1 @@
+#picturetuner
---
+++ main.js
@@ -0,0 +1,107 @@
+var notes = ['-','-','-','-','-','-'];
+var red=70;
+var green=70;
+var blue=70;
+path1 ="cover.jpg";
+var write = function(){
+		$(".e").append(notes[0]+"-");
+		$(".B").append(notes[1]+"-");
+		$(".G").append(notes[2]+"-");
+		$(".D").append(notes[3]+"-");
+		$(".A").append(notes[4]+"-");
+		$(".Ee").append(notes[5]+"-");
+		notes = ['-','-','-','-','-','-'];
+	
+};
+
+var fun = function(){
+	console.log("y0");
+	for (var i = 1; i <= 200; i++) {
+    var tick = function(i) {
+        return function() {
+            write();
+        }
+    };
+    setTimeout(tick(i), 1000 * i);
+}
+};
+
+
+var image = function() {
+              path1 ="cover.jpg";
+              var img = new Image();
+              img.src = path1;
+              img.crossOrigin="Anonymous";
+              var c=document.getElementById("layer1");
+              var ctx=c.getContext("2d");
+              ctx.clearRect(0, 0, 1100, 500);
+              img.addEventListener("load", function() {
+                 // execute drawImage statements here
+                 ctx.drawImage(img,0,0,  img.naturalWidth, img.naturalHeight, 
+                                  0,0,700,               500);
+              }, false);
+          
+
+    };
+
+var color = function(){
+			path1 ="cover.jpg";
+			var img = new Image();
+            img.src = path1;
+            img.crossOrigin="Anonymous";
+            var num1= red;
+            var num2= green;
+            var num3= blue;
+            console.log(red, green, blue);
+            var color = "rgba("+num1+","+num2+","+num3+", .2)";
+            var c=document.getElementById("layer1");
+            var ctx=c.getContext("2d");
+           ctx.clearRect(0, 0, 1100, 500);
+            img.addEventListener("load", function() {
+                // execute drawImage statements here
+                ctx.drawImage(img,0,0,  img.naturalWidth, img.naturalHeight, 
+                                  0,0, 700,               500);
+            }, false);
+            ctx.globalCompositeOperation = "multiply";
+            ctx.fillStyle = color;
+            for (var i =0; i<5; i++){
+            
+                 ctx.fillRect(0,0,700,500);
+            }
+}
+var scale = function(value1, value2, valuex, valuey){
+			var img = new Image();
+            img.src = path1;
+		var c=document.getElementById("layer1");
+		var ctx=c.getContext("2d");
+		//ctx.translate(valuex,valuey);
+		ctx.clearRect(0, 0, 1100, 500);
+            img.addEventListener("load", function() {
+                // execute drawImage statements here
+                ctx.drawImage(img,0,0,  img.naturalWidth, img.naturalHeight, 
+                                  0,0, 700,               500);
+            }, false);
+            ctx.scale(value1, value2);
+            img.addEventListener("load", function() {
+                // execute drawImage statements here
+                ctx.drawImage(img,0,0,  img.naturalWidth, img.naturalHeight, 
+                                  0,0, 700,               500);
+            }, false);
+
+}
+/*
+var rotate = function(value){
+			var img = new Image();
+            img.src = path1;
+			var c=document.getElementById("layer1");
+			var ctx=c.getContext("2d");
+			ctx.clearRect(0, 0, 1100, 500);
+			ctx.rotate(value * Math.PI / 180);
+            img.addEventListener("load", function() {
+                // execute drawImage statements here
+                ctx.drawImage(img,0,0,  img.naturalWidth, img.naturalHeight, 
+                                  0,0, 700,               500);
+            }, false);
+
+}*/
+$(document).ready(image);
